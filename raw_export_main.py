08123900e1154c3e8467cfe0c5847e2e12af09d2
Kai Domhardt <kai.domhardt@gmail.com>
2015-03-17 15:30:01 +0100

    Added input structure for type selection

--- raw_export_main.py
+++ raw_export_main.py
@@ -1,9 +1,10 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-d 
 #!/usr/bin/python
 
 import sqlite3
 import os
-from raw_export import exportMultipleItems 
+import shutil
+from raw_export_base import exportMultipleItems 
 
 
 
@@ -32,17 +33,46 @@ def exportCollection(currentCollection,exportPath,storagePath,cursor):
             exportCollection(childCollection,exportPath,storagePath,cursor)
 
 
+def exportByTags():
+    print("I DO STUFF")
+
+
 
 if __name__ == '__main__':
-    STORAGEPATH=os.getcwd()+"\\storage\\"
-    EXPORTBASEPATH = raw_input("Enter the path you want to export to:\n")
+    #TODO: Collection needs to be validatedadd a
+    #TODO: implement preset default export path
+    
+    validinput = False
+    while validinput == False:
+        STORAGEPATH=os.getcwd()+"\\storage\\"
+        EXPORTBASEPATH = raw_input("Enter the path you want to export to:\n")
+        if os.path.isdir(EXPORTBASEPATH):
+            validinput = True
+        else:
+            print ("WARNING: Invalid Path!")
+            
     shutil.copy2("zotero.sqlite","zotero_workcopy.sqlite")
     conn = sqlite3.connect('zotero_workcopy.sqlite')
     cursor = conn.cursor()
-    CollectionToExport = raw_input("Enter the name of the collection you would like to export:\n")
-    sql = "SELECT collectionID FROM collections WHERE collectionName = \""+CollectionToExport+"\""
-    cursor.execute(sql)
-    startingCollection = cursor.fetchone()
-    exportCollection(startingCollection,EXPORTBASEPATH,STORAGEPATH,cursor)
+            
+    validinput = False
+    while validinput == False:
+        print("1: Collection")
+        print("2: Tags")
+        exporttype = raw_input("Enter the type of export:")
+        
+        if exporttype == "1":
+            validinput = True
+            CollectionToExport = raw_input("Enter the name of the collection you would like to export:\n")
+            sql = "SELECT collectionID FROM collections WHERE collectionName = \""+CollectionToExport+"\""
+            cursor.execute(sql)
+            startingCollection = cursor.fetchone()
+            exportCollection(startingCollection,EXPORTBASEPATH,STORAGEPATH,cursor)
+        elif exporttype == "2":
+            validinput = True
+            exportByTags() #TODO: Add Parameters
+        else:
+            print ("WARNING: Invalid Input!")
+        
     conn.close()
     print("Export Complete")
